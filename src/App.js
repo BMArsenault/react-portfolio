71fe20da2196f9c4fa77790add6b21e84e7c6f59
Brian <BMArsenault@gmail.com>
2022-02-24 00:07:21 -0700

    Added to index.js in components folders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import About from './components/About';
+import Nav from './components/Nav';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('About');
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'About': 
+        return <About/>;
+      case 'Resume': 
+        return <Resume/>;
+      case 'Portfolio':
+        return <Portfolio/>;
+      case 'Contact':
+        return <Contact/>;
+      default: 
+        return <About/>;
+    }
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Home from "../Home";
+
+function Portfolio() {
+
+    const [project] = useState([
+        {
+            name: 'The Hype',
+            description: 'j;lkasdf',
+            link: "https://github.com",
+            repo: "https://github.com"
+        },
+        {
+            name: 'Hike Spot',
+            description: 'j;lkasdf',
+            link: "https://github.com",
+            repo: "https://github.com"
+        },
+        {
+            name: 'j;laksdf',
+            description: 'j;lkasdf',
+            link: "https://github.com",
+            repo: "https://github.com"
+        },
+        {
+            name: 'j;laksdf',
+            description: 'j;lkasdf',
+            link: "https://github.com",
+            repo: "https://github.com"
+        },
+        {
+            name: 'j;laksdf',
+            description: 'j;lkasdf',
+            link: "https://github.com",
+            repo: "https://github.com"
+        },
+    ]);
+
+    return (
+        <div>
+            <div className="flex-row">
+                {projects.map((project, idx) => (
+                    <Project
+                    project={project}
+                    key={"project" + idx}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
