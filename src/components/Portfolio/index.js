d38e67f4697c67ee96211fc8d52803a9ab3e4e5d
Brian <BMArsenault@gmail.com>
2022-02-24 00:29:40 -0700

    Set up project and portfolio components

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,36 +1,41 @@
 import React, { useState } from 'react';
-import Home from "../Home";
+import Project from "../Project";
 
 function Portfolio() {
 
-    const [home] = useState([
+    const [project] = useState([
         {
             name: 'The Hype',
             description: 'j;lkasdf',
-            link: "https://github.com",
+            image: require('../../assets/img/'),
+            link: "https://github.io",
             repo: "https://github.com"
         },
         {
             name: 'Hike Spot',
             description: 'j;lkasdf',
-            link: "https://github.com",
+            image: require('../../assets/img/'),
+            link: "https://github.io",
             repo: "https://github.com"
         },
         {
             name: 'j;laksdf',
             description: 'j;lkasdf',
-            link: "https://github.com",
+            image: require('../../assets/img/'),
+            link: "https://github.io",
             repo: "https://github.com"
         },
         {
             name: 'j;laksdf',
             description: 'j;lkasdf',
-            link: "https://github.com",
+            image: require('../../assets/img/'),
+            link: "https://github.io",
             repo: "https://github.com"
         },
         {
             name: 'j;laksdf',
             description: 'j;lkasdf',
+            image: require('../../assets/img/'),
             link: "https://github.com",
             repo: "https://github.com"
         },
---
+++ src/components/Project/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Project = ({project}) => {
+    return(
+        <div className='col-12 col-md-6 col-lg-4 p-3' data-aos="fade-up">
+        <div className='card h-100'>
+            <img className='project card-img-top' src={project.image} alt="projects"/>
+            <div className='card-body text-center'>
+                <h5 className='card-title'><u>{project.name}</u></h5>
+                <p className='card-text'>{project.description}</p>
+            </div>
+            <a href={project.link} target="_blank" className='btn btn-primary'><i class='bx bx-link' ></i>View Project</a>
+            <a href={project.repo} target="_blank" className='btn btn-primary'><i class='bx bxl-github'></i>View Repo</a>
+        </div>
+    </div>
+    )
+}
+
+export default Project;
